725d0b2c40e40a301ae1cb2352108a4fd07d170b
Eduardo S. Pereira <pereira.somoza@gmail.com>
2018-06-12 20:28:35 -0300

    Adicionando video streaming

--- robot.py
+++ robot.py
@@ -1,10 +1,9 @@
 import time
 from flask import Flask, url_for, render_template
-from flask import request
-
-
-
-from servo import Servo 
+from flask import request, Response
+import cv2
+from numpy import array
+from servo import Servo
 
 app = Flask(__name__, static_url_path = "", static_folder = "static" )
 
@@ -12,6 +11,27 @@ app.secret_key = 'X456yhj3k510oq'
 servobase = Servo(18)
 servoaltura = Servo(19)
 servoinclinacao = Servo(17)
+
+def gen():
+    CAM = cv2.VideoCapture(0)
+    while True:
+        ret, image = CAM.read()
+        frame = b''
+        if image is not None:
+            f = "./static/images/tmp/frame.jpeg"
+            cv2.imwrite(f, image)
+            frame = open(f, 'rb').read()
+            #ret, frame = cv2.imencode('.png', image)
+            #frame = array(frame).tostring()
+        
+        yield (b'--frame\r\n' +
+               b'Contetn-Type:image/jpeg\r\n\r\n' +
+               frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(), 
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route("/arm", methods=["GET"])
@@ -54,7 +74,9 @@ def home():
 
 
 if __name__ == "__main__":
+    
     app.run(debug=True, host="0.0.0.0", port=8080)
+    
 
 
 
